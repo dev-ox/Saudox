59e7fc373f251c08d46c0823a04b43b0dfa2827d
Matheus Machado <matheusmachado708@gmail.com>
2020-05-14 00:52:46 -0300

    Adicionando os models com as migrations

---
+++ Saudox/app/Anamnese_Gigante_Psicopeda_Neuro_Psicomoto_pt3.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Anamnese_Gigante_Psicopeda_Neuro_Psicomoto_pt3 extends Model
+{
+    //
+}
---
+++ Saudox/database/migrations/2020_05_13_000008_create_convenios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConveniosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('convenios', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+
+            $table->string('nome_convenio');
+            $table->unsignedInteger('numero_convenio');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('convenios');
+    }
+}
