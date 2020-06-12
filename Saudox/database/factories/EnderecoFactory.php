0d2c6775ecaba1059eca67d69c26d17955435a22
Carlos Antônio <carlosaajunior.jp@gmail.com>
2020-06-12 19:02:15 -0300

    Factorys para os testes

---
+++ Saudox/database/factories/EnderecoFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Endereco;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Endereco::class, function (Faker $faker) {
+    return [
+                'estado' => 'MG',
+                'cidade' => 'Joao Pinheiro',
+                'ponto_referencia' => 'Favela',
+    ];
+});
