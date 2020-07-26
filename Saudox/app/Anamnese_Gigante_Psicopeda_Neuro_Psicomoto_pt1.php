1c2f48583df13f551833fffb9af91dfb415b94be
Matheus Machado <matheusmachado708@gmail.com>
2020-07-25 23:30:35 -0300

    Acesso aos documentos

--- Saudox/app/Anamnese_Gigante_Psicopeda_Neuro_Psicomoto_pt1.php
+++ Saudox/app/Anamnese_Gigante_Psicopeda_Neuro_Psicomoto_pt1.php
@@ -7,4 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 class Anamnese_Gigante_Psicopeda_Neuro_Psicomoto_pt1 extends Model
 {
     protected $table = 'anamnese__pnp__pt1s';
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id_pode_ver' => 'array',
+        'id_pode_editar' => 'array',
+    ];
+
+
+    /* TODO: Lembrar disso quando for criar/editar */
+    /* Pra setar o acesso:
+     * $arr é um array com os ids dos pacientes que podem ver;
+     * $anamnese->id_pode_ver["paciente"] = json_encode($arr);
+     * Pra verificar o acesso:
+     * $arr = json_decode($anamnese->id_pode_ver["paciente"]);
+     * E mesma coisa pra id_pode_editar.
+     */
 }
