2d8134483d74689248fee8b277423332c14959ac
Matheus Machado <matheusmachado708@gmail.com>
2020-08-17 00:03:05 -0300

    Criando algumas poucas coisas Rotas pro profissional ver paciente

--- Saudox/app/Http/Controllers/Profissional/ProfissionalController.php
+++ Saudox/app/Http/Controllers/Profissional/ProfissionalController.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Profissional;
+use App\Paciente;
 use Auth;
 
 class ProfissionalController extends Controller
@@ -23,16 +24,33 @@ class ProfissionalController extends Controller
     public function home() {
         $profissional = Profissional::find(Auth::id());
         $profissoes = $profissional->getProfissoes();
-		return view('profissional/home', ['profissoes' => $profissoes]);
+
+        // TODO: A ideia é de alguma forma entregar só os pacientes que se tem interesse
+        $pacientes = Paciente::all();
+
+        return view('profissional/home',
+            [
+                'profissoes' => $profissoes,
+                'pacientes' => $pacientes
+            ]);
 	}
 
 
-    public function ver_profissional($id) {
+    public function verProfissional($id) {
         $profissional = Profissional::find($id);
         if($profissional){
             return view('profissional/ver_profissional', ['profissional' => $profissional]);
         } else {
             return redirect()->route('erro', ['msg_erro' => "Profissional inexistente"]);
+        }
+    }
+
+    public function verPaciente($id) {
+        $paciente = Profissional::find($id);
+        if($paciente){
+            return view('profissional/ver_paciente', ['paciente' => $paciente]);
+        } else {
+            return redirect()->route('erro', ['msg_erro' => "Paciente inexistente"]);
         }
     }
 
