772fcd44e9f350901f792ad5cf9ceb7d5ab0a0cc
Carlos Antônio <carlosaajunior.jp@gmail.com>
2020-08-31 12:39:49 -0300

    Retirando os 'prints' e colocando chaves no lugar

--- Saudox/resources/views/profissional/home.blade.php
+++ Saudox/resources/views/profissional/home.blade.php
@@ -1,5 +1,7 @@
- @include('layouts.layoutperfil')
+@include('layouts.layoutperfil')
+<meta content="width=device-width, initial-scale=1" name="viewport" />
 
+<div class="desktop">
 <head>
   <div class="welcome">
     <h1>Bem vindo! {{$profissional->nome}} </h1>
@@ -18,27 +20,24 @@
       <label class="infosprox"> Hora de Saída: </label>
       <label class="infosprox"> Local de atendimento: </label>
       <h4 class="tdclien">
-      <?php
-      print($agenda[0]->nome);
-      ?>
+      {{$agenda[0]->nome}}
+
     </h4>
 
       <h4 class="tdclien-hora">
-      <?php
-      print($agenda[0]->data_entrada)
-      ?>
+      {{$agenda[0]->data_entrada}}
     </h4>
 
     <h4 class="tdclien-datasaida">
-    <?php
-    print($agenda[0]->data_saida)
-    ?>
+
+    {{$agenda[0]->data_saida}}
+
   </h4>
 
   <h4 class="tdclien-local">
-  <?php
-    print($agenda[0]->local_de_atendimento)
-  ?>
+
+    {{$agenda[0]->local_de_atendimento}}
+
   </h4>
     <div class="btpc">
     @if($prox_paciente)
@@ -51,18 +50,6 @@
     </div>
   </div>
 </body>
-
-
-<!--
-<h3>
-<?php
-    echo("Profissões: ");
-    foreach($profissoes as $p){
-        echo($p."\n");
-    }
-?>
-</h3>
--->
 
 <div class="agenda">
 <h3 class="agnd">Agenda:</h3>
@@ -101,4 +88,104 @@
 
 <div class="logo-image">
       <img class="empresa" src="https://avatars3.githubusercontent.com/u/64805526?s=400&u=e4188ff9af3c0927962a181f241fbee79c506f4d&v=4">
+</div>
+<div class="adm-bt">
+@if(in_array('Admin', $profissoes))
+  <a class="btn-adm" href=<?php route('profissional.admin.dashboard') ?>> Ir para administração </a>
+@endif
+</div>
+</div>
+
+
+
+
+
+<div class="celular">
+
+<div class="welcome-phone">
+  <h1>Bem vindo! {{$profissional->nome}} </h1>
+  <div class="adm-bt-phone">
+    <a class="btn-perfil-phone" href="/profissional/perfil"> Ver Perfil </a>
+  @if(in_array('Admin', $profissoes))
+    <a class="btn-perfil-phone" href=<?php route('profissional.admin.dashboard') ?>> Ir para administração </a>
+  @endif
+  </div>
+</div>
+
+  <div class="prox-phone">
+    <h3 class="clien-phone"> Próximo Cliente: </h3>
+    <div class="info-phone-prox">
+    <label class="infosprox-phone"> Nome: </label>
+    <h4 class="tdclien-phone">
+
+    {{ $agenda[0]->nome }}
+
+    </h4>
+
+    <label class="infosprox-phone"> Hora de Entrada: </label>
+    <h4 class="tdclien-phone">
+
+    {{ $agenda[0]->data_entrada }}
+
+    </h4>
+
+    <label class="infosprox-phone"> Hora de Saída: </label>
+    <h4 class="tdclien-phone">
+
+    {{ $agenda[0]->data_saida }}
+
+    </h4>
+
+    <label class="infosprox-phone"> Local de atendimento: </label>
+    <h4 class="tdclien-phone">
+
+      {{ $agenda[0]->local_de_atendimento }}
+
+    </h4>
+  </div>
+
+  <div class="btpc-phone">
+  @if($prox_paciente)
+    <a class="btn-paciente-phone" href=<?php route('paciente.perfil', ['id' => $prox_paciente->id]) ?>> Ver perfil </a>
+  @else
+    <a class="btn-paciente-phone"  href=<?php route('register') ?>> Registrar Cliente  </a>
+  @endif
+  </div>
+
+  </div>
+
+  <div class="agenda-phone">
+    <h3 class="agnd-phone">Agenda:</h3>
+    @if (count($agenda) > 0)
+    <div class="table-wrapper-phone">
+        <table class="ag table-phone">
+        <thead>
+          <tr class="ag table-row-phone">
+             <th class="tag-phone">Paciente:</th>
+             <th class="tag-phone">Hora Entrada:</th>
+             <th class="tag-phone">Hora Saída:</th>
+             <th class="tag-phone">Local do atendimento:</th>
+             <th class="tag-phone">Tipo da Recorrência:</th>
+             <th class="tag-phone">Recorrência do Agendamento:</th>
+          </tr>
+        </thead>
+
+        <tbody>
+        @foreach ($agenda as $ag)
+            <tr>
+                <td class="corsim-phone"> {{$ag->nome}} </td>
+                <td class="corsim-phone"> {{$ag->data_entrada}} </td>
+                <td class="corsim-phone"> {{$ag->data_saida}} </td>
+                <td class="corsim-phone"> {{$ag->local_de_atendimento}} </td>
+                <td class="corsim-phone"> {{$ag->tipo_da_recorrencia}} </td>
+                <td class="corsim-phone"> {{$ag->recorrencia_do_agendamento}} </td>
+            </tr>
+        @endforeach
+        </tbody>
+        </table>
+    </div>
+        @else
+                <p>Você não possui nenhum agendamento</p>
+        @endif
+  </div>
 </div>
