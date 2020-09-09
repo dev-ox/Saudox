b56b52f8af54a5769fc427edc6de64bd82b1cd7c
Matheus Machado <matheusmachado708@gmail.com>
2020-09-09 16:12:59 -0300

    Melhorando a view de erro

--- Saudox/resources/views/erro.blade.php
+++ Saudox/resources/views/erro.blade.php
@@ -1,7 +1,28 @@
-<h1>Erro!</h1>
+<style>
+.caixotao {
+    border: 2px solid #FFCC33;
+    width: 50%;
+    margin: auto;
+    margin-top: 15%;
+    color: white;
+    background: #424242;
+    text-align: center;
+}
 
-<?php
+.caixotao a {
+    text-decoration: none;
+    color: white;
+}
 
-echo("<h3>".$msg_erro."</h3>");
+</style>
 
-?>
+
+
+<div class="caixotao">
+    <h1>Erro!</h1>
+    <div>
+        {{ $msg_erro }}
+    </div>
+    <br>
+    <a href="/"><h3>Clique aqui para voltar ao inicio</h3></a>
+</div>
