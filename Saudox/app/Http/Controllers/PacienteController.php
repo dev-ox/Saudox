5561d8ffb8d5682860ba92d4c5108bf83792fd94
Carlos Antônio <carlosaajunior.jp@gmail.com>
2020-10-01 13:31:23 -0300

    [#174982748] Home de paciente pronta, mas feia

--- Saudox/app/Http/Controllers/PacienteController.php
+++ Saudox/app/Http/Controllers/PacienteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Agendamentos;
 use App\Paciente;
 use Auth;
 
@@ -16,7 +17,40 @@ class PacienteController extends Controller {
 
     // Redireciona para a home do profissional
     public function home(){
-        return view('paciente.home');
+        $paciente = Paciente::find(Auth::id());
+
+        $ava_fono = $paciente->avaliacao_fono;
+        $ava_judo = $paciente->avaliacao_judo;
+        $ava_neur = $paciente->avaliacao_neuro;
+        $ava_tocp = $paciente->avaliacao_terapia_ocupacional;
+
+        $anamnese_fono = $paciente->anamnese__terapia__ocupacionals;
+        $anamnese_pnps = $paciente->anamneseFonoaudiologias;
+        $anamnese_tocp = $paciente->anamneseGigantePsicopedaNeuroPsicomotos();
+
+        $agendamentos = Agendamentos::where('status', '1')
+            ->where('cpf', $paciente->cpf)
+            //Odenados pela data de entrada
+            ->orderBy('data_entrada', 'asc')
+            ->get();
+
+        if($paciente){
+            return view('paciente.home', [
+                'paciente' => $paciente,
+                'agendamentos' => $agendamentos,
+                'ava_fono' => $ava_fono,
+                'ava_judo' => $ava_judo,
+                'ava_neur' => $ava_neur,
+                'ava_tocp' => $ava_tocp,
+                'anamnese_fono' => $anamnese_fono,
+                'anamnese_pnps' => $anamnese_pnps,
+                'anamnese_tocp' => $anamnese_tocp,
+            ]);
+        } else {
+            return redirect()->route('erro', ['msg_erro' => "Paciente inexistente"]);
+        }
+
     }
+
 
 }
