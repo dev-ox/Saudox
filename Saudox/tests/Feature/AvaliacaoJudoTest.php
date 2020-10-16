d92db584a72d59b7aa324827b2c04e47387ba394
MBaltz <mateusbaltazar9@gmail.com>
2020-10-16 09:56:19 -0300

    [#174990436] Testes de Edição de Avaliacao Judo

--- Saudox/tests/Feature/AvaliacaoJudoTest.php
+++ Saudox/tests/Feature/AvaliacaoJudoTest.php
@@ -31,7 +31,45 @@ class AvaliacaoJudoTest extends TestCase {
             'login' => 'login_teste',
             'password' => $this->password,
         ]);
-        $this->evolucao_judo = factory(AvaliacaoJudo::class)->create([
+
+        $this->array_ava_judo = [
+            'id_paciente' => $this->paciente->id,
+            'id_profissional' => $this->profissional->id,
+            'diagnostico' => 4,
+            'relacao_com_as_pessoas_judo' => 4,
+            'resposta_emocional' => 4,
+            'uso_do_corpo' => 4,
+            'uso_de_objetos' => 4,
+            'adaptacao_a_mudancas' => 4,
+            'resposta_auditiva' => 4,
+            'resposta_visual' => 4,
+            'medo_ou_nervosismo' => 4,
+            'comunicacao_verbal' => 4,
+            'orienta_se_por_som' => 4,
+            'comunicacao_nao_verbal' => 4,
+            'orienta_se_por_som' => 4,
+            'reacao_ao_nao' => 4,
+            'compreendem_comandos_simples_palavras_que_descrevam_objetos' => 4,
+            'manipula_brinquedos_objetos' => 4,
+            'equilibrio' => 4,
+            'forca' => 4,
+            'resistencia' => 4,
+            'marcha' => 4,
+            'agilidade' => 4,
+            'coordenacao_motora_fina' => 4,
+            'coordenacao_motora_grossa' => 4,
+            'propriocepcao' => 4,
+            'compreende_direcoes' => 4,
+            'compreende_comandos_professoras' => 4,
+            'concentracao' => 4,
+            'comportamento_reflexo' => 4,
+            'observacoes' => 4,
+            'terapias' => 4,
+            'objetivos' => 4,
+            'tipo_da_aula' => 4,
+        ];
+
+        $this->avaliacao_judo = factory(AvaliacaoJudo::class)->create([
             'id_paciente' => $this->paciente->id,
             'id_profissional' => $this->profissional->id,
         ]);
@@ -89,7 +127,7 @@ class AvaliacaoJudoTest extends TestCase {
         $resposta_ver_terapiaOcupacional->assertSee("Resposta emocional");
 
         // Gera uma cópia da anamnese da Factory, indicando os ids
-        $copia_anamnese = array($this->evolucao_judo);
+        $copia_anamnese = array($this->avaliacao_judo);
         $copia_anamnese['id_paciente'] = $paciente_aux->id;
         $copia_anamnese['id_profissional'] = $prof_aux->id;
 
@@ -115,5 +153,32 @@ class AvaliacaoJudoTest extends TestCase {
             $resposta_ver_ava_judo = $this->get(route("profissional.avaliacao.judo.ver", ['id_paciente' => $this->paciente->id]));
             $resposta_ver_ava_judo->assertSee("Comportamento reflexivo");
     }
+
+    /** @test **/
+    /* url: https://www.pivotaltracker.com/story/show/174990436 */
+    /* TA_01 */
+    public function profissionalPodeEditarAvaliacaoJudo() {
+        // Gera um profissional com as profissões indicadas e realiza o login
+        $criarProf_Logar = $this->criarProfELogar(
+            array(
+                Profissional::Fonoaudiologo,
+                Profissional::TerapeutaOcupacional,
+            ), $this->password);
+            $prof_aux = $criarProf_Logar['profissional'];
+
+
+            // Gera uma cópia da anamnese da Factory, indicando os ids
+            $copia_ava = array($this->avaliacao_judo);
+            $copia_ava['id_paciente'] = $this->paciente->id;
+            $copia_ava['id_profissional'] = $this->profissional->id;
+            $copia_ava['diagnostico'] = 9;
+
+            // Cria a Anamnese
+            $res = $this->post(route("profissional.avaliacao.judo.editar.salvar", $copia_ava));
+
+            $resposta_ver_ava_judo = $this->get(route("profissional.avaliacao.judo.ver", ['id_paciente' => $this->paciente->id]));
+            $resposta_ver_ava_judo->assertSee("9");
+
+        }
 
 }
