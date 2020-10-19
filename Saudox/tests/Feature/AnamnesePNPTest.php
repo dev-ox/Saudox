74cff2c509c1570e8d0c335c3639b61351625490
MBaltz <mateusbaltazar9@gmail.com>
2020-10-19 04:30:40 -0300

    Teste para editar anamnese PNP

--- Saudox/tests/Feature/AnamnesePNPTest.php
+++ Saudox/tests/Feature/AnamnesePNPTest.php
@@ -93,18 +93,18 @@ class AnamnesePNPTest extends TestCase {
 
         $this->assertCount(0, AnamneseGigantePsicopedaNeuroPsicomoto::all());
         $pnp = factory(AnamneseGigantePsicopedaNeuroPsicomoto::class)->create();
-        $arr_pt1 = factory(AnamneseGigantePsicopedaNeuroPsicomotoPt1::class)->create([
+        $arr_pt1 = array(factory(AnamneseGigantePsicopedaNeuroPsicomotoPt1::class)->create([
             'id_paciente' => $this->paciente->id,
             'id_profissional' => $this->profissional->id,
             'id_tp' => $pnp->id,
             'compareceram_entrevista' => "Joao de Deuso",
-        ]);
-        $arr_pt2 = factory(AnamneseGigantePsicopedaNeuroPsicomotoPt2::class)->create([
+        ]));
+        $arr_pt2 = array(factory(AnamneseGigantePsicopedaNeuroPsicomotoPt2::class)->create([
             'id_tp' => $pnp->id
-        ]);
-        $arr_pt3 = factory(AnamneseGigantePsicopedaNeuroPsicomotoPt3::class)->create([
+        ]));
+        $arr_pt3 = array(factory(AnamneseGigantePsicopedaNeuroPsicomotoPt3::class)->create([
             'id_tp' => $pnp->id
-        ]);
+        ]));
         $this->assertCount(1, AnamneseGigantePsicopedaNeuroPsicomoto::all());
 
         $pnp_array_merged = $arr_pt1;
@@ -116,12 +116,10 @@ class AnamnesePNPTest extends TestCase {
         $resposta_ver_fono->assertSee("Joao de Deuso");
 
         $pnp_array_merged['compareceram_entrevista'] = "Maria de Deuso";
-        $resposta_ver_fono = $this->get(route("profissional.anamnese.psicopedagogia.editar.salvar", $pnp_array_merged));
-        $resposta_ver_fono->assertSee("Joao de Deuso");
-
-        // Verifica se a anamnese agora existe
-        $resposta_ver_fono = $this->get(route("profissional.anamnese.psicopedagogia.editar", ['id_paciente' => $this->paciente->id]));
+        $resposta_ver_fono = $this->post(route("profissional.anamnese.psicopedagogia.editar.salvar", $pnp_array_merged));
+        $resposta_ver_fono->assertSessionHasNoErrors();
         $resposta_ver_fono->assertSee("Maria de Deuso");
+
     }
 
 }
