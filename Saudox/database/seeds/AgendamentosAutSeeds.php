a9022eafbda22ad693b80c6ac07c395ce2b7f1bc
ednaldoj <ednaldo.dantas.jr@gmail.com>
2020-10-26 14:58:13 -0300

    Ajustes observações agendamento

--- Saudox/database/seeds/AgendamentosAutSeeds.php
+++ Saudox/database/seeds/AgendamentosAutSeeds.php
@@ -4,7 +4,6 @@ use Illuminate\Database\Seeder;
 use Carbon\Carbon;
 
 class AgendamentosAutSeeds extends Seeder {
-
     public function run() {
         include('database/seeds/SeedsConfig.php');
         include_once('database/seeds/FuncoesAuxuliaresSeeds.php');
@@ -12,9 +11,12 @@ class AgendamentosAutSeeds extends Seeder {
         $nome = ['Maria', 'Miguel', 'Alice', 'Arthur', 'Laura', 'Heitor', 'Manuela', 'Bernardo', 'Valentina', 'Davi', 'Shophia', 'Pedro', 'Júlia', 'João', 'Lívia', 'Lucas', 'Beatriz', 'Gustavo', 'Maria Clara', 'Murilo', 'Beatriz', 'Isaac'];
         $sobrenome = ['Monteiro da Silva', 'Alves de Melo', 'Dias Cardoso', 'Ferreira dos Santos', 'da Silva Nascimento', 'Medeiros de Lima', 'Vieira Nazário', 'Dantas de Oliveira', 'Rodrigues dos Santos', 'Souza Pereira', 'Gomes Ribeiro', 'Martins Andrade'];
         $local_de_atendimento = ['Clínica', 'Residência do paciente'];
+        $tipo_da_recorrencia = ['Primeira vez', 'Frequente'];
+        $observacoes = ['Sem observações', 'Glicose baixa ao nascer'];
 
         //Gerando anamnese terapia ocupacional automaticamente
         for($i = 0; $i < $qtd_agendamentos; $i++){
+
             DB::table('agendamentos')->insert([
                 'id_convenio' => rand(1,$qtd_convenios),
                 'id_profissional' => rand(1,$qtd_profissionals),
@@ -28,8 +30,8 @@ class AgendamentosAutSeeds extends Seeder {
                 'data_saida' => Carbon::now()->format('Y-m-d H:i:s'),
                 'local_de_atendimento' => $local_de_atendimento[rand(0, sizeof($local_de_atendimento)-1)],
                 'recorrencia_do_agendamento' => rand(0,1) >= 0.5,
-                'tipo_da_recorrencia' => Str::random(10),
-                'observacoes' => texto(5),
+                'tipo_da_recorrencia' => $tipo_da_recorrencia[rand(0, sizeof($tipo_da_recorrencia)-1)],
+                'observacoes' => $observacoes[rand(0, sizeof($observacoes)-1)],
                 'status' => rand(0,1) >= 0.3,
             ]);
         }
